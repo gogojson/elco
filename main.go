b37ecd571208410b327a89920315f3b885729366
J Son <go.jhson@gmail.com>
2025-01-02 11:03:30 +0900

    add test handler

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"log/slog"
 	"net/http"
+	"strconv"
 )
 
 func main() {
 	slog.Info("Check if program starts")
 
 	handler := http.DefaultServeMux
+
+	handler.HandleFunc("GET /quiz/{id}", getQuiz)
 
 	api := http.Server{
 		Addr:    "0.0.0.0:8822",
@@ -25,5 +29,20 @@ func main() {
 	case err := <-serverError:
 		slog.Error("server error", slog.String("errorMessage", err.Error()))
 	}
+
+}
+
+func getQuiz(w http.ResponseWriter, r *http.Request) {
+	//TODO: Validate if id is type int
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil {
+		w.WriteHeader(http.StatusNotAcceptable)
+		w.Write([]byte(fmt.Sprintf("Invalid param: %s", r.PathValue("id"))))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+	w.Write([]byte(fmt.Sprintf("This is the path value: %d", id)))
 
 }
